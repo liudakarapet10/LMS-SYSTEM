0f4b1cd3e1d05998d061bff4d30fcc514c04a491
liudakarapet <liudakarapet10@gmail.com>
2023-10-28 19:06:55 +0300

    change navItem element
    removed url param and added onKeyDown function for switch menu item when press on arrow buttons

--- app/components/navItem.tsx
+++ app/components/navItem.tsx
@@ -1,15 +1,35 @@
+import React, { KeyboardEvent } from 'react';
+
 interface NavItemProps {
   text: string;
-  url: string;
   id?: string;
   isActive: boolean;
   onClick?: () => void;
+  onKeyDown?: React.KeyboardEventHandler<HTMLAnchorElement>; // Correct type
+  className?: string;
+  tabIndex?: number;
 }
 
-export function NavItem({ text, url, isActive = false, id, onClick }: NavItemProps) {
+export function NavItem({
+  text,
+  isActive = false,
+  id,
+  onClick,
+  onKeyDown,
+  tabIndex,
+}: NavItemProps) {
   return (
     <li className={`w-full ${isActive ? 'text-[#2464EB] focus-visible:outline-none' : 'cursor-pointer hover:text-[#2464EB]'}`}>
-      <a className={`block w-full ${isActive ? 'focus-visible:outline-none':  "" }`} href={url} aria-current={isActive ? 'page' : undefined} id={id} onClick={onClick}>{text}</a>
+      <a
+        className={`block w-full ${isActive ? 'focus:outline-none' : ''}`}
+        aria-current={isActive ? 'page' : undefined}
+        id={id}
+        onClick={onClick}
+        onKeyDown={onKeyDown} // Use the correct type here
+        tabIndex={tabIndex}
+      >
+        {text}
+      </a>
     </li>
   );
 }
